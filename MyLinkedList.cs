c3e160fd23bea65d39e8ec95b598d649b8ead4b2
Mikey Teal <Mikey.Teal@softwire.com>
2018-01-12 15:51:24 +0000

    Add project files.

---
+++ MyLinkedList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLists
+{
+    public class MyLinkedList
+    {
+        private LinkedList<string> list = new LinkedList<string>();
+
+        public LinkedList<string> getList()
+        {
+            return list;
+        }
+
+        public void createNewList()
+        {
+            add("Fred");
+            add("Wilma");
+            add("Betty");
+            add("Barney");
+        }
+
+        public void add(string s)
+        {
+            list.AddLast(s);
+        }
+
+        public LinkedListNode<string> getNodeOfStringIfInList(string s)
+        {
+            foreach(var value in list)
+            {
+                if(value == s)
+                {
+                    return (list.Find(s));
+                }
+            }
+
+            return null;
+        }
+
+        public void deleteNodeWithString(string s)
+        {
+            LinkedListNode<string> targetNode = list.Find(s);
+            LinkedListNode<string> previousNode = null;
+            LinkedListNode<string> currentNode = list.First;
+
+            while (currentNode != null)
+            {
+                if (currentNode == targetNode)
+                {
+                    list.Remove(currentNode);
+                }
+                else
+                {
+                    previousNode = currentNode;
+                }
+                currentNode = currentNode.Next;
+            }
+        }
+
+        public string[] getStringArrayOfValues()
+        {
+            return list.ToArray();
+        }
+    }
+}
---
+++ MyQueue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLists
+{
+    public class MyQueue
+    {
+        private Queue<string> queue = new Queue<string>();
+
+        public Queue<string> getQueue()
+        {
+            return queue;
+        }
+
+        public void createNewQueue()
+        {
+            add("Fred");
+            add("Wilma");
+            add("Betty");
+            add("Barney");
+        }
+
+        public void add(string s)
+        {
+            queue.Enqueue(s);
+        }
+
+        public int getPositionOfStringIfInList(string s)
+        {
+            Queue<string> tempQueue = queue;
+            int counter = 0;
+            if (queue.Contains(s))
+            {
+                foreach (var value in tempQueue)
+                {
+                    if (value == s)
+                    {
+                        return counter;
+                    }
+                    counter++;
+                }
+            }
+            return 99999;
+        }
+
+        public void deleteItemWithValue(string s)
+        {
+            Queue<string> newQueue = new Queue<string>();
+            int counter = 0;
+
+            foreach (var value in queue)
+            {
+                if (value != s || counter > 0)
+                {
+                    newQueue.Enqueue(value);
+                }
+                if (value == s)
+                {
+                    counter++;
+                }
+            }
+
+            queue = newQueue;
+        }
+
+        public string[] getStringArrayOfValues()
+        {
+            return queue.ToArray();
+        }
+    }
+}
